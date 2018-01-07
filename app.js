e6d1247658003e77453909527c3a288bb4a975fb
jpwebdev <jjpwebdev@gmail.com>
2018-01-06 23:42:56 -0500

    Spotify API working

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const client = new Twitter(myKeys.twitter);
 const spotify = new Spotify(myKeys.spotify);
 
 
-
-const myTweets = () => { //This will show your last 20 tweets and when they were created at in your terminal/bash window.
+// Twitter API - Displays your last 20 tweets and when they were created at in your terminal/bash window.
+const myTweets = () => { 
     const params = {    // Setting up the query paramaters
         screen_name: 'JeanPaulG_', // Query Search
     };
@@ -31,23 +31,36 @@ const myTweets = () => { //This will show your last 20 tweets and when they were
     });
 }
 
+// Spotify API - Function that displays: Artist, Album and the preview url from a song that the user chooses
 const mySpotify = (userInput) => {
-    let userInputArr = [];  // Holds the user input (name of song) from the command line
-    let userInputStr = '';
+    let userInputArr = [];  // Holds all arguments passed by the user in the command line
+    let userInputStr = '';  // Holds all arguments as one string
+
+    // Converts the array of arguments into one string to be used as a query search string
     for ( let i = 3; i < userInput.length; i++) {
         userInputArr.push(userInput[i]);
         userInputStr = userInputArr.join(' ');
     }
-    console.log(userInputStr);
-    spotify.search({ type: 'track', query: userInputStr }, (err, data) => {
-        if (err) { return console.log('Error occurred: ' + err); };
-        //console.log(data.tracks.items[0].album.name);
-        for(let j = 0; j < data.tracks.total; j++) {
-            console.log(data.tracks.items[j].album.name);
-        };
-        //console.log(JSON.stringify(data, null, 2)); 
-    });
 
+    // Spotify API to search for song
+    spotify.search({ type: 'track', query: userInputStr }, (err, data) => {
+        
+        if (err) { return console.log('Error occurred: ' + err); }; // If there's an error show it
+
+        let totalResults = data.tracks.total;   // Get the number of results from the response object
+        console.log(`Results Found: ${totalResults}`);  // Let the user know how many results where found
+
+        for(let j = 0; j < totalResults; j++) { // Looping through the response JSON to display specific info
+            console.log(`
+                Result #${j+1}:
+                Song: ${data.tracks.items[j].name}
+                Artist: ${data.tracks.items[j].album.artists[0].name}
+                Album: ${data.tracks.items[j].album.name}
+                Preview: ${data.tracks.items[j].preview_url}
+            `);
+            
+        };
+    });
 }
 
 // Get user arguments from the command line and call the appropiate function 
