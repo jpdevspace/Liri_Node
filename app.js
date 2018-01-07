a620a58bbc927ac58eecb8ab90e5e56d41299e8a
jpwebdev <jjpwebdev@gmail.com>
2018-01-06 23:18:39 -0500

    Working on the Spotify API: Navigating through the object

--- app.js
+++ app.js
@@ -31,13 +31,32 @@ const myTweets = () => { //This will show your last 20 tweets and when they were
     });
 }
 
+const mySpotify = (userInput) => {
+    let userInputArr = [];  // Holds the user input (name of song) from the command line
+    let userInputStr = '';
+    for ( let i = 3; i < userInput.length; i++) {
+        userInputArr.push(userInput[i]);
+        userInputStr = userInputArr.join(' ');
+    }
+    console.log(userInputStr);
+    spotify.search({ type: 'track', query: userInputStr }, (err, data) => {
+        if (err) { return console.log('Error occurred: ' + err); };
+        //console.log(data.tracks.items[0].album.name);
+        for(let j = 0; j < data.tracks.total; j++) {
+            console.log(data.tracks.items[j].album.name);
+        };
+        //console.log(JSON.stringify(data, null, 2)); 
+    });
+
+}
+
 // Get user arguments from the command line and call the appropiate function 
 switch(process.argv[2]) {
     case 'my-tweets':
         myTweets();
         break;
     case 'spotify-this-song':
-        deposit(process.argv[3]);
+        mySpotify(process.argv);
         break;
     case 'movie-this':
         withdraw(process.argv[3]);
